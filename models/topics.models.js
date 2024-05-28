45f8903dd50af7364096fb7251bc37ada29829d7
Tom Follows <tomjfollows@gmail.com>
2024-05-28 13:09:12 +0100

    this will complete all task 1 set ups along with the completion of task 2 '2-get-topics'

---
+++ models/topics.models.js
@@ -0,0 +1,10 @@
+const db = require("../db/connection");
+
+exports.fetchTopics= () => {
+  return db.query("SELECT * FROM topics").then((result) => {
+    const topics = result.rows;
+    if (!topics) return Promise.reject({ status: 404, msg: "Not found" });
+    return topics;
+  });
+  
+};
