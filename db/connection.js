02ead071979b4275f6dfe661dc58be271ca47ebd
Tom Follows <tomjfollows@gmail.com>
2024-05-31 12:02:15 +0100

    env amendment

--- db/connection.js
+++ db/connection.js
@@ -7,13 +7,15 @@ require('dotenv').config({
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
-};
+}
 
 const config = {};
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+} else {
+  config.database = process.env.PGDATABASE;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
