fd7b95f50299362e6021800644f7e9dc56908456
Tom Follows <tomjfollows@gmail.com>
2024-05-31 11:01:38 +0100

    this will complete ticket12-comment-count

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -79,6 +79,27 @@ describe("GET /api/articles/:article_id", () => {
         });
       });
   });
+
+  test("200: GET articles by the article id and responds with an article object, which should have the following properties: author, title, article_id, body, topic, created_at, votes, article_img_url, comment_count", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        expect(article).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: "11",
+        });
+      });
+  });
   test("404: will respond with a 404 when the server cannot find the requested resource", () => {
     return request(app)
       .get("/api/articles/999")
--- models/articles.models.js
+++ models/articles.models.js
@@ -6,10 +6,18 @@ exports.getArticles = () => {
   });
 };
 
-exports.SelectByArticleId = (article_id) => {
+exports.SelectByArticleId = (id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .query(
+      `SELECT articles.*, COUNT(comments.comment_id) AS comment_count
+    FROM articles 
+    left join comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id`,
+      [id]
+    )
     .then((result) => {
+      console.log(result.rows);
       if (result.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Route Not Found" });
       } else {
@@ -118,4 +126,3 @@ exports.VotesModel = (article_id, inc_votes) => {
       return res.rows[0];
     });
 };
-
